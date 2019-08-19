0df65fdb645db29a1a87aadc4c141063e03003c7
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-19 14:05:50 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 
 def meal_choice(veg1, veg2, protein = "meat")
   p "What a nutritious meal!"
-  p"A plate #{protein} with #{veg1} and #{veg2}."
+  p "A plate #{protein} with #{veg1} and #{veg2}."
 end
 
 meal_choice
